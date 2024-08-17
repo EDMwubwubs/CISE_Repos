c01cb1e4576fd94ddf01c88fa159d8f83aa7c46a
edmwubwubs <dannyeda555@gmail.com>
2024-08-17 21:33:33 +1200

    commit all folders to github

--- worksheet2/my-react-app/src/App.test.tsx
+++ worksheet2/my-react-app/src/App.test.tsx
@@ -11,9 +11,4 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-// New test case for addition
-describe('Addition', () => {
-  it('knows that 2 and 2 make 4', () => {
-    expect(2 + 2).toBe(4);
-  });
-});
+
